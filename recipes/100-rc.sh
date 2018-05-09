4def22a86891bd44868697ef5189494cdcd417b8
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-05-09 23:37:56 +0900

    Add sym link to rc/stdlib.sh

--- recipes/100-rc.sh
+++ recipes/100-rc.sh
@@ -6,6 +6,7 @@ readonly RC_REPO="$HOME/rc"
 info "Linking dot files from kexplo/rc..."
 
 git_repo https://github.com/kexplo/rc.git "$RC_REPO"
+sym_link "$RC_REPO/stdlib.sh" "$HOME/stdlib.sh"
 sym_link "$RC_REPO/.vimrc" "$HOME/.vimrc"
 sym_link "$RC_REPO/.zshrc" "$HOME/.zshrc"
 sym_link "$RC_REPO/.gitconfig" "$HOME/.gitconfig"
