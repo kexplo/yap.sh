3e074a6fed06ed95b2b926a4b84c14f78f645188
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-05-13 22:40:04 +0900

    Remove two variables

--- recipes/200-linuxbrew.sh
+++ recipes/200-linuxbrew.sh
@@ -5,8 +5,6 @@ if [[ "$(uname -s)" == "Linux" ]]; then
 
   # Set Linuxbrew's variables to prevent install failing
   export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-  export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
-  export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
 
   yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 fi
