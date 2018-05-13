6fca5d387b9d75be4935916b3845c279292d40f9
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-05-13 22:34:31 +0900

    Set Linuxbrew environment variables

--- recipes/200-linuxbrew.sh
+++ recipes/200-linuxbrew.sh
@@ -2,6 +2,11 @@
 
 if [[ "$(uname -s)" == "Linux" ]]; then
   info "Installing linuxbrew...."
+
+  # Set Linuxbrew's variables to prevent install failing
+  export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+  export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
+  export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
+
   yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-  PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 fi
