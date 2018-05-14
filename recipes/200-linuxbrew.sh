3e6c0b353c30c6e5d3705b1a959470b3eed03983
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-05-15 00:17:20 +0900

    Add Linuxbrew path before installing

--- recipes/200-linuxbrew.sh
+++ recipes/200-linuxbrew.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+
 if ! has brew; then
   if [[ "$(uname -s)" == "Linux" ]]; then
     info "Installing linuxbrew...."
-
-    # Set Linuxbrew's variables to prevent install failing
-    export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 
     set +o pipefail  # prevent to failing yap.sh
     yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
