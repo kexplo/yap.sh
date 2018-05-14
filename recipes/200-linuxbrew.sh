4ae2245e42cec2bf780f1f56c162ac3aebaee9da
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-05-15 00:12:18 +0900

    Skip install Linuxbrew if installed

--- recipes/200-linuxbrew.sh
+++ recipes/200-linuxbrew.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ "$(uname -s)" == "Linux" ]]; then
-  info "Installing linuxbrew...."
+if ! has brew; then
+  if [[ "$(uname -s)" == "Linux" ]]; then
+    info "Installing linuxbrew...."
 
-  # Set Linuxbrew's variables to prevent install failing
-  export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+    # Set Linuxbrew's variables to prevent install failing
+    export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 
-  set +o pipefail  # prevent to failing yap.sh
-  yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-  set -o pipefail
+    set +o pipefail  # prevent to failing yap.sh
+    yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    set -o pipefail
+  fi
+else
+  info "Skipped, Linuxbrew already installed"
 fi
