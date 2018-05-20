efb52a32f0aeab6045f05379c7325b0c4aa4f70a
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-05-21 00:58:04 +0900

    Add coverage pass rule

--- coverage.sh
+++ coverage.sh
@@ -13,20 +13,47 @@ no_ubuntu_16_recipes=()
 no_ubuntu_18_recipes=()
 no_osx_recipes=()
 
+is_osx () {
+  [[ "$(uname)" == "Darwin" ]]
+}
+
+increase_all_count () {
+  ubuntu_16_count=$((ubuntu_16_count+1))
+  ubuntu_18_count=$((ubuntu_18_count+1))
+  osx_count=$((osx_count+1))
+}
+
+pass_os_check () {
+  local os="$1"
+  local recipe_path="$2"
+  local mangled_os
+  if is_osx; then
+    mangled_os=$(echo "$os" | sed -E 's/\ /_/; s/\./_/')
+  else
+    mangled_os=$(echo "$os" | sed -r 's/\s/_/; s/\./_/')
+  fi
+  grep -q "# pass os: $os" "$recipe_path" || grep -q "is_${mangled_os}" "$recipe_path"
+}
+
 for recipe in $yapsh_dir/recipes/*; do
   if [[ -f "$recipe" ]]; then
     _basename="$(basename "$recipe")"
-    if grep -q 'is_ubuntu_16_04' "$recipe"; then
+    if pass_os_check "all" "$recipe"; then
+      recipe_count=$((recipe_count+1))
+      increase_all_count
+      continue
+    fi
+    if pass_os_check "ubuntu 16.04" "$recipe"; then
       ubuntu_16_count=$((ubuntu_16_count+1))
     else
       no_ubuntu_16_recipes+=("$_basename")
     fi
-    if grep -q 'is_ubuntu_18_04' "$recipe"; then
+    if pass_os_check "ubuntu 18.04" "$recipe"; then
       ubuntu_18_count=$((ubuntu_18_count+1))
     else
       no_ubuntu_18_recipes+=("$_basename")
     fi
-    if grep -q 'is_osx' "$recipe"; then
+    if pass_os_check "osx" "$recipe"; then
       osx_count=$((osx_count+1))
     else
       no_osx_recipes+=("$_basename")
