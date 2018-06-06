1bb9ed420c67bc4504b0f9e7bdd9c07c9f276e7a
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-06-07 02:03:21 +0900

    Use pushd-popd

--- recipes/420-vim.sh
+++ recipes/420-vim.sh
@@ -23,25 +23,26 @@ function install_vim () {
   version_regex="${version/./\\.}"
   # Clone latest revision of version
   git clone -b "$(git ls-remote --tags https://github.com/vim/vim.git | grep -o "v${version_regex}"'\.[0-9]\+' | tail -1)" --depth 1 https://github.com/vim/vim.git /tmp/vim
-  cd '/tmp/vim'
-  install_vim_dependencies
+  pushd '/tmp/vim'
+    install_vim_dependencies
 
-  # NOTE: If both python2.x and python3.x are enabled then the linking will be via dlopen(), dlsym(), dlclose().
-  # SEE: https://github.com/vim/vim/blob/master/src/Makefile#L444-L445
-  ./configure --with-features=huge \
-    --enable-multibyte \
-    --enable-rubyinterp=yes \
-    --enable-pythoninterp=yes \
-    --with-python-command=/usr/bin/python2 \
-    --enable-perlinterp=dynamic \
-    --enable-luainterp=yes \
-    --with-luajit \
-    --enable-cscope \
-    --enable-fail-if-missing \
-    --prefix=/usr/local
-  # --enable-python3interp=yes \
-  # --with-python3-command=/usr/bin/python3 \
-  sudo make install
+    # NOTE: If both python2.x and python3.x are enabled then the linking will be via dlopen(), dlsym(), dlclose().
+    # SEE: https://github.com/vim/vim/blob/master/src/Makefile#L444-L445
+    ./configure --with-features=huge \
+      --enable-multibyte \
+      --enable-rubyinterp=yes \
+      --enable-pythoninterp=yes \
+      --with-python-command=/usr/bin/python2 \
+      --enable-perlinterp=dynamic \
+      --enable-luainterp=yes \
+      --with-luajit \
+      --enable-cscope \
+      --enable-fail-if-missing \
+      --prefix=/usr/local
+    # --enable-python3interp=yes \
+    # --with-python3-command=/usr/bin/python3 \
+    sudo make install
+  popd
   sudo rm -rf /tmp/vim
 }
 
