1741e4c51bb466ab976995463b742975c69ff913
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-06-07 01:10:07 +0900

    Install Vim 8.1 from source

--- recipes/420-vim.sh
+++ recipes/420-vim.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-function install_vim () {
+function install_vim_dependencies() {
   if is_ubuntu_16_04; then
     local pkgs=(
-      vim
-      vim-nox-py2
+      lua5.1
+      liblua5.1-dev
+      luajit
+      libluajit-5.1-dev
+      ruby-dev
+      libperl-dev
+      ncurses-dev
       mono-xbuild  # for YouCompleteMe
       cmake        # for YouCompleteMe
     )
@@ -12,8 +17,34 @@ function install_vim () {
   fi
 }
 
+function install_vim () {
+  version="$1"
+  version_regex="${version/./\\.}"
+  # Clone latest revision of version
+  git clone -b "$(git ls-remote --tags https://github.com/vim/vim.git | grep -o "v${version_regex}"'\.[0-9]\+' | tail -1)" --depth 1 https://github.com/vim/vim.git /tmp/vim
+  cd '/tmp/vim'
+  install_vim_dependencies
 
-install_vim
+  # NOTE: If both python2.x and python3.x are enabled then the linking will be via dlopen(), dlsym(), dlclose().
+  # SEE: https://github.com/vim/vim/blob/master/src/Makefile#L444-L445
+  ./configure --with-features=huge \
+    --enable-multibyte \
+    --enable-rubyinterp=yes \
+    --enable-pythoninterp=yes \
+    --with-python-command=/usr/bin/python2 \
+    --enable-perlinterp=dynamic \
+    --enable-luainterp=yes \
+    --with-luajit \
+    --enable-cscope \
+    --enable-fail-if-missing \
+    --prefix=/usr/local
+  # --enable-python3interp=yes \
+  # --with-python3-command=/usr/bin/python3 \
+  sudo make install
+  sudo rm -rf /tmp/vim
+}
+
+install_vim 8.1
 
 if [ ! -f ~/.vim/autoload/plug.vim ]
 then
