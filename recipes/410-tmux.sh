4972d8f229d724625287d00d61b1f356a2dee367
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-06-23 00:29:19 +0900

    Install tmux from source

--- recipes/410-tmux.sh
+++ recipes/410-tmux.sh
@@ -1,9 +1,51 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if is_ubuntu_16_04; then
-  sudo apt-get install -y tmux
-fi
+# if is_ubuntu_16_04; then
+#   sudo apt-get install -y tmux
+# fi
+
+function install_tmux_dependencies() {
+  if is_ubuntu_16_04; then
+    local pkgs=(
+      automake
+      build-essential
+      pkg-config
+      libevent-dev
+      libncurses5-dev
+    )
+    sudo apt-get install -y "${pkgs[@]}"
+  fi
+}
+
+function check_tmux_installed() {
+  version="$1"
+  if ! has tmux; then
+    false
+  fi
+  tmux -V | grep -q "tmux $version"
+}
+
+function install_tmux() {
+  local version="$1"
+
+  if check_tmux_installed "$version"; then
+    echo "tmux $version already installed"
+    return
+  fi
+
+  mkdir -p /tmp/tmux
+  wget -qO- "https://github.com/tmux/tmux/releases/download/${version}/tmux-${version}.tar.gz" | tar xvz -C /tmp/tmux
+  pushd /tmp/tmux/tmux*
+   ./configure
+   make
+   sudo make install
+  popd
+  rm -rf /tmp/tmux
+}
+
+install_tmux_dependencies
+install_tmux 2.7
 
 info "Installing tpm"
 github_repo tmux-plugins/tpm ~/.tmux/plugins/tpm
