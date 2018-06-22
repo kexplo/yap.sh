6313f15cf64373178513f123f63d11e244557ca3
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-06-23 01:33:34 +0900

    Remove python@2 brew package after fpp installed

--- recipes/202-fpp.sh
+++ recipes/202-fpp.sh
@@ -4,3 +4,9 @@ set -euo pipefail
 
 info "Installing facebook's PathPicker(fpp)..."
 brew_inst fpp
+
+# fix YCM import error.
+# SEE: https://github.com/Valloric/YouCompleteMe/issues/2883
+if brew ls --versions 'python@2' >/dev/null; then
+  brew uninstall --ignore-dependencies python@2
+fi
