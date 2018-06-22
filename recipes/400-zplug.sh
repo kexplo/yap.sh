05ce1bcfc38d447c2be5f11b5c53476a03b71bd3
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-06-23 02:38:07 +0900

    Fix ensure_zplug_plugins

--- recipes/400-zplug.sh
+++ recipes/400-zplug.sh
@@ -12,15 +12,19 @@ install_zplug() {
   github_repo zplug/zplug "$HOME/.zplug"
 }
 
+ensure_zplug_plugins() {
+  # Ensure the latest zplug plugins installed
+  zsh -c "
+  source \"$HOME/.zshrc\"
+  if ! zplug check; then
+    zplug install
+  else
+    zplug update
+  fi
+  "
+}
+
 set_zsh_as_default_shell
 install_zplug
+ensure_zplug_plugins
 
-# Ensure the latest zplug plugins installed
-zsh -c "
-source \"$HOME/.zplug/init.zsh\"
-if ! zplug check; then
-  zplug install
-else
-  zplug update
-fi
-"
