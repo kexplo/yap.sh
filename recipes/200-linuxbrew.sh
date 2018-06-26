2176c3584623f077d3854834a2db419434004593
Chanwoong Kim <chanwoong@chanwoong.kim>
2018-06-26 22:10:50 +0900

    Set linuxbrew install code to always successul

--- recipes/200-linuxbrew.sh
+++ recipes/200-linuxbrew.sh
@@ -9,9 +9,7 @@ if ! has brew; then
   if [[ "$(uname -s)" == "Linux" ]]; then
     info "Installing linuxbrew...."
 
-    set +o pipefail  # prevent to failing yap.sh
-    yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-    set -o pipefail
+    (yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)") || true
   fi
 else
   info "Skipped, Linuxbrew already installed"
